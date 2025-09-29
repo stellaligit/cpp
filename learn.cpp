a278ea9485c25e5efa5f3332d898eac2b2c06fc4
stellaligit <68718208+stellaligit@users.noreply.github.com>
2025-09-29 18:31:24 -0400

    Create learn.cpp
    day 1 relearning c++, hoping to do ccc senior using c++ this year

---
+++ learn.cpp
@@ -0,0 +1,17 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main_1() {
+    cout << 3-4 << endl;
+    cout << 5*6 << endl;
+    cout << 8/4 << endl;
+    cout << fixed << setprecision(2) << (float) 8/5 << endl;
+    return 0;
+}
