e1d1ace8eaea7b7951b708a01b227b4346ff1036
stellaligit <stella.li.365@gmail.com>
2025-11-05 13:57:26 -0500

    Update learn.cpp

--- learn.cpp
+++ learn.cpp
@@ -385,3 +385,18 @@ int main_20() {
     cout << s;
     return 0;
 }
+
+// 100 to 999
+int main_20a() {
+    for (int a = 1; a <= 9; a++) {
+        for (int b = 0; b <= 9; b++) {
+            for (int c = 0; c <= 9; c++) {
+                int num = 100*a + 10*b + c;
+                if (num == pow(a, 3) + pow(b, 3) + pow(c, 3)) {
+                    cout << num << endl;
+                }
+            }
+        }
+    }
+    return 0;
+}
