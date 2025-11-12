7c3e553ec98d2d5955b7dfa11ae6f6a9757d7351
stellaligit <stella.li.365@gmail.com>
2025-11-12 10:16:29 -0500

    Update learn.cpp

--- learn.cpp
+++ learn.cpp
@@ -502,3 +502,18 @@ int main_22a() {
     }
     return 0;
 }
+
+int main_22b() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j<((n-i)*2-1); j++) {
+            cout << "#";
+        }
+        cout << endl;
+    }
+    return 0;
+}
